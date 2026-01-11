01c459b4600b74d1e3d1894269168634c14f99e9
11968-ship-it <11968@holbertonstudents.com>
2026-01-11 19:46:32 +0300

    Update marks.c

--- marks.c
+++ marks.c
@@ -3,18 +3,29 @@
 #include <stdio.h>
 
 /**
-*
-*
-*
+  *
+  *
+  *
+  *
 */
-{
 
-for(i = 0; i < 5; i++)
+void input_marks(float marks[])
 {
-printf("Enter mark for subject %d: ", i+1);
-scanf("%f", &marks[i]);
-printf("You entered: %.2f\n", marks[i]);
+int i;
+float sum = 0;
+
+/* loop from i = 0 to subject -1 */
+for(i = 0; i < SUBJECTS; i++)
+{
+  printf("Enter mark for subject %d: ", i+1);
+  scanf("%f", &marks[i]);
+  printf("You entered: %.2f\n", marks[i]);
 }
 
-return (0);
+float calculate_avg(const float marks[]);
+for(i = 0; i < SUBJECTS; i++)
+{
+  /* add each mark to sum */
+  sum += marks[i];
+}
 }
