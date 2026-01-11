113fdab12293d4d3b7ba8f14c5b32cc66a7b083a
11968-ship-it <11968@holbertonstudents.com>
2026-01-11 19:35:44 +0300

    Update marks.c

--- marks.c
+++ marks.c
@@ -7,10 +7,14 @@
 *
 *
 */
-int main(void)
 {
-float marks[SUBJECTS];
-input_marks(marks);
+
+for(i = 0; i < 5; i++)
+{
+printf("Enter mark for subject %d: ", i+1);
+scanf("%f", &marks[i]);
+printf("You entered: %.2f\n", marks[i]);
+}
 
 return (0);
 }
