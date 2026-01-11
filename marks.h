7130777fb8a3c30d47135f67099e89190ce221c6
11968-ship-it <11968@holbertonstudents.com>
2026-01-11 18:40:30 +0300

    Update marks.h

--- marks.h
+++ marks.h
@@ -1,6 +1,15 @@
 #ifndef MARKS_H
 #define MARKS_H
 
+/* A function that fills the marks array */
+
+/* A function that calculates the average */
+
+/* A function that finds the highest mark */
+
+/* A function that finds the lowest mark */
+
+/* A function that determines the grade */
 
 
 #endif
