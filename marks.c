3189cb0cb263d39e7700919b0ea9b4f684040683
11968-ship-it <11968@holbertonstudents.com>
2026-01-11 19:32:21 +0300

    Update marks.c

--- marks.c
+++ marks.c
@@ -1,10 +1,16 @@
 #include "marks.h"
 /* scanf and printf */
-#include "stdio.h"
+#include <stdio.h>
 
 /**
 *
 *
 *
 */
+int main(void)
+{
+float marks[SUBJECTS];
+input_marks(marks);
 
+return (0);
+}
