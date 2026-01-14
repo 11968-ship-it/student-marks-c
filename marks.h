91f2061cdc76037fe3520f2baf590bdf5fb92a62
11968-ship-it <11968@holbertonstudents.com>
2026-01-14 15:15:26 +0300

    Update marks.h

--- marks.h
+++ marks.h
@@ -8,9 +8,9 @@ void input_marks(float marks[]);
 /* A function that calculates the average */
 float calculate_avg(const float marks[]);
 /* A function that finds the highest mark */
-float find_highest_mark(float marks[]);
+float find_highest_mark(const float marks[]);
 /* A function that finds the lowest mark */
-float find_lowest_mark(float marks[]);
+float find_lowest_mark(const float marks[]);
 /* A function that determines the grade */
 char calculate_grade(float average);
 
